293dafd413544b39716a7648e2270b3311706693
Connor Sullivan <connor.sullivan@fcsamerica.com>
2019-08-01 14:43:03 -0500

    Added Garden Grid competent

---
+++ WebUI/src/app/home/garden-grid/garden-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-garden-grid',
+  templateUrl: './garden-grid.component.html',
+  styleUrls: ['./garden-grid.component.scss']
+})
+export class GardenGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- WebUI/src/app/home/home.module.ts
+++ WebUI/src/app/home/home.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { GardenGridComponent } from './garden-grid/garden-grid.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, GardenGridComponent],
   imports: [
     CommonModule
   ],
